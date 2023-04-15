b0250ab7a4ac219e2e0f65abf25682542397b28e
Mathieu Thériault <>
2023-04-15 17:47:05 -0400

    Commands changes and port change to 8080

--- src/main.ts
+++ src/main.ts
@@ -2,26 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as helmet from 'helmet';
 import { ValidationPipe } from '@nestjs/common';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
-  const config = new DocumentBuilder()
-    .setTitle('Api Simulation de stage')
-    .setDescription('API AEC 2023')
-    .setVersion('1.0')
-    .addTag('stage')
-    .addBearerAuth(undefined, 'defaultBearerAuth')
-    .build();
-
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
 
   // @ts-ignore
   app.use(helmet());
   app.useGlobalPipes(new ValidationPipe({
     enableDebugMessages: true,
   }));
-  await app.listen(3000);
+  await app.listen(process.env.PORT || "8080");
 }
 bootstrap();
