9918c5e3ca8f254ed67ae39ce37d8de4c5e27340
Mathieu Thériault <>
2023-04-15 18:05:58 -0400

    Changed port

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,6 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe({
     enableDebugMessages: true,
   }));
-  await app.listen(process.env.PORT || "8080");
+  await app.listen(8080);
 }
 bootstrap();
