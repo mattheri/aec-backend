bf0903ea5dcb1ee4c3451f550422bb7d0759d569
Mathieu Theriault <mathieu.theriault89@gmail.com>
2023-04-27 16:19:49 -0400

    Fix possible crash

--- src/expenses/expenses.service.ts
+++ src/expenses/expenses.service.ts
@@ -8,7 +8,7 @@ import { UsersService } from 'src/users/users.service';
 import { ExpenseQueryFilter } from './entities/expense.entity';
 import { UpdateManyExpenseDto } from './dto/update-many-expense.dto';
 import { UserErrorCodes } from 'src/users/interfaces/users.interfaces';
-import { Expense as ExpenseEntity } from "./entities/expense.entity";
+import { Expense as ExpenseEntity } from './entities/expense.entity';
 import { ExpenseErrorCodes } from './interfaces/expense.interface';
 
 @Injectable()
@@ -16,84 +16,145 @@ export class ExpensesService {
   constructor(
     @InjectModel(Expense.name) private expenseModel: Model<Expense>,
     private readonly userService: UsersService,
-  ) { }
+  ) {}
 
   async create(createExpenseDto: CreateExpenseDto) {
-    if (!createExpenseDto.user) throw new HttpException(UserErrorCodes.NO_ID_PROVIDED, HttpStatus.BAD_REQUEST);
-    if (!await this.userService.exists({ _id: createExpenseDto.user })) throw new HttpException(UserErrorCodes.USER_NOT_FOUND, HttpStatus.NOT_FOUND);
+    if (!createExpenseDto.user)
+      throw new HttpException(
+        UserErrorCodes.NO_ID_PROVIDED,
+        HttpStatus.BAD_REQUEST,
+      );
+    if (!(await this.userService.exists({ _id: createExpenseDto.user })))
+      throw new HttpException(
+        UserErrorCodes.USER_NOT_FOUND,
+        HttpStatus.NOT_FOUND,
+      );
 
-    const expense = (await this.expenseModel.create(createExpenseDto)).toObject();
+    const expense = (
+      await this.expenseModel.create(createExpenseDto)
+    )?.toObject();
+
+    if (!expense)
+      throw new HttpException(
+        'Error creating expense',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
     return new ExpenseEntity(expense);
   }
 
   async createMany(createExpenseDto: CreateExpenseDto[]) {
-    if (!createExpenseDto.some(async (e) => await this.userService.exists({ _id: e.user }))) throw new HttpException(UserErrorCodes.USER_NOT_FOUND, HttpStatus.NOT_FOUND);
+    if (
+      !createExpenseDto.some(
+        async (e) => await this.userService.exists({ _id: e.user }),
+      )
+    )
+      throw new HttpException(
+        UserErrorCodes.USER_NOT_FOUND,
+        HttpStatus.NOT_FOUND,
+      );
 
     const expenses = await this.expenseModel.create(createExpenseDto);
 
     return expenses.map((e) => new ExpenseEntity(e.toObject()));
   }
 
-  async findAll(
-    user: string,
-    { limit, offset, sort }: ExpenseQueryFilter,
-  ) {
-    if (!await this.userService.exists({ _id: user })) {
-      throw new HttpException("User not found", HttpStatus.NOT_FOUND);
+  async findAll(user: string, { limit, offset, sort }: ExpenseQueryFilter) {
+    if (!(await this.userService.exists({ _id: user }))) {
+      throw new HttpException(
+        UserErrorCodes.USER_NOT_FOUND,
+        HttpStatus.NOT_FOUND,
+      );
     }
 
     const query = this.expenseModel.find({ user, archived: false });
     if (limit) query.limit(limit);
     if (offset) query.skip(offset);
     if (sort) {
-      const [field, order] = sort.split(":");
-      if (order === "asc") query.sort({ [field]: 1 });
+      const [field, order] = sort.split(':');
+      if (order === 'asc') query.sort({ [field]: 1 });
       else query.sort({ [field]: -1 });
     }
 
-    return (await query.exec()).map(e => new ExpenseEntity(e.toObject()));
+    return (await query.exec()).map((e) => new ExpenseEntity(e.toObject()));
   }
 
   async findOne(id: string) {
-    const expense = (await this.expenseModel.findById(id).exec()).toObject();
-    if (!expense) throw new HttpException(ExpenseErrorCodes.EXPENSE_NOT_FOUND, HttpStatus.NOT_FOUND);
+    const expense = (await this.expenseModel.findById(id).exec())?.toObject();
+    if (!expense)
+      throw new HttpException(
+        ExpenseErrorCodes.EXPENSE_NOT_FOUND,
+        HttpStatus.NOT_FOUND,
+      );
 
     return new ExpenseEntity(expense);
   }
 
   async update(id: string, updateExpenseDto: UpdateExpenseDto) {
-    const expense = (await this.expenseModel.findByIdAndUpdate(id, updateExpenseDto, { new: true }).exec()).toObject();
-    if (!expense) throw new HttpException(ExpenseErrorCodes.EXPENSE_NOT_FOUND, HttpStatus.NOT_FOUND);
+    const expense = (
+      await this.expenseModel
+        .findByIdAndUpdate(id, updateExpenseDto, { new: true })
+        .exec()
+    )?.toObject();
+    if (!expense)
+      throw new HttpException(
+        ExpenseErrorCodes.EXPENSE_NOT_FOUND,
+        HttpStatus.NOT_FOUND,
+      );
 
     return new ExpenseEntity(expense);
   }
 
   updateMany(updateManyExpenseDto: UpdateManyExpenseDto[]) {
-    return Promise.all(updateManyExpenseDto.map(async (e) => {
-      const exists = await this.expenseModel.exists({ _id: e._id });
-      if (!exists) throw new HttpException(ExpenseErrorCodes.EXPENSE_NOT_FOUND, HttpStatus.NOT_FOUND);
+    return Promise.all(
+      updateManyExpenseDto.map(async (e) => {
+        const exists = await this.expenseModel.exists({ _id: e._id });
+        if (!exists)
+          throw new HttpException(
+            ExpenseErrorCodes.EXPENSE_NOT_FOUND,
+            HttpStatus.NOT_FOUND,
+          );
 
-      const expense = (await this.expenseModel.findByIdAndUpdate(e._id, e, { new: true }).exec()).toObject();
+        const expense = (
+          await this.expenseModel
+            .findByIdAndUpdate(e._id, e, { new: true })
+            .exec()
+        ).toObject();
 
-      return new ExpenseEntity(expense);
-    }));
+        return new ExpenseEntity(expense);
+      }),
+    );
   }
 
   async remove(id: string) {
     const exists = await this.expenseModel.exists({ _id: id });
-    if (!exists) throw new HttpException(ExpenseErrorCodes.EXPENSE_NOT_FOUND, HttpStatus.NOT_FOUND);
+    if (!exists)
+      throw new HttpException(
+        ExpenseErrorCodes.EXPENSE_NOT_FOUND,
+        HttpStatus.NOT_FOUND,
+      );
 
-    const expense = (await this.expenseModel.findByIdAndUpdate(id, { archived: true }, { new: true }).exec()).toObject();
+    const expense = (
+      await this.expenseModel
+        .findByIdAndUpdate(id, { archived: true }, { new: true })
+        .exec()
+    ).toObject();
     return new ExpenseEntity(expense);
   }
 
   async removeMany(ids: string[]) {
-    return Promise.all(ids.map(async (id) => {
-      const exists = await this.expenseModel.exists({ _id: id });
-      if (!exists) throw new HttpException("Expense not found", HttpStatus.NOT_FOUND);
+    return Promise.all(
+      ids.map(async (id) => {
+        const exists = await this.expenseModel.exists({ _id: id });
+        if (!exists)
+          throw new HttpException('Expense not found', HttpStatus.NOT_FOUND);
 
-      const expense = (await this.expenseModel.findByIdAndUpdate(id, { archived: true }, { new: true }).exec()).toObject();
-      return new ExpenseEntity(expense);
-    }));
+        const expense = (
+          await this.expenseModel
+            .findByIdAndUpdate(id, { archived: true }, { new: true })
+            .exec()
+        ).toObject();
+        return new ExpenseEntity(expense);
+      }),
+    );
   }
 }
--- src/incomes/incomes.service.ts
+++ src/incomes/incomes.service.ts
@@ -7,7 +7,7 @@ import { Model } from 'mongoose';
 import { UsersService } from 'src/users/users.service';
 import { IncomeErrorCodes, IncomeQueryFilter } from './entities/income.entity';
 import { UpdateManyIncomeDto } from './dto/update-many-expense.dto';
-import { Income as IncomeEntity } from "./entities/income.entity";
+import { Income as IncomeEntity } from './entities/income.entity';
 import { UserErrorCodes } from 'src/users/interfaces/users.interfaces';
 
 @Injectable()
@@ -15,36 +15,56 @@ export class IncomesService {
   constructor(
     @InjectModel(Income.name) private incomeModel: Model<Income>,
     private userService: UsersService,
-  ) { }
+  ) {}
 
   async create(createIncomeDto: CreateIncomeDto) {
-    if (!createIncomeDto.user) throw new HttpException(UserErrorCodes.NO_ID_PROVIDED, HttpStatus.BAD_REQUEST);
-    if (!await this.userService.exists({ _id: createIncomeDto.user })) throw new HttpException(UserErrorCodes.USER_NOT_FOUND, HttpStatus.NOT_FOUND);
+    if (!createIncomeDto.user)
+      throw new HttpException(
+        UserErrorCodes.NO_ID_PROVIDED,
+        HttpStatus.BAD_REQUEST,
+      );
+    if (!(await this.userService.exists({ _id: createIncomeDto.user })))
+      throw new HttpException(
+        UserErrorCodes.USER_NOT_FOUND,
+        HttpStatus.NOT_FOUND,
+      );
 
-    const income = (await this.incomeModel.create(createIncomeDto)).toObject();
+    const income = (await this.incomeModel.create(createIncomeDto))?.toObject();
+    if (!income)
+      throw new HttpException(
+        'Error creating income',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
     return new IncomeEntity(income);
   }
 
   async createMany(createIncomeDto: CreateIncomeDto[]) {
-    if (!createIncomeDto.some(async (e) => await this.userService.exists({ _id: e.user }))) throw new HttpException(UserErrorCodes.USER_NOT_FOUND, HttpStatus.NOT_FOUND);
+    if (
+      !createIncomeDto.some(
+        async (e) => await this.userService.exists({ _id: e.user }),
+      )
+    )
+      throw new HttpException(
+        UserErrorCodes.USER_NOT_FOUND,
+        HttpStatus.NOT_FOUND,
+      );
 
-    return (await this.incomeModel.create(createIncomeDto)).map((e) => new IncomeEntity(e.toObject()));
+    return (await this.incomeModel.create(createIncomeDto)).map(
+      (e) => new IncomeEntity(e.toObject()),
+    );
   }
 
-  async findAll(
-    user: string,
-    { limit, offset, sort }: IncomeQueryFilter,
-  ) {
-    if (!await this.userService.exists({ _id: user })) {
-      throw new HttpException("User not found", HttpStatus.NOT_FOUND);
+  async findAll(user: string, { limit, offset, sort }: IncomeQueryFilter) {
+    if (!(await this.userService.exists({ _id: user }))) {
+      throw new HttpException('User not found', HttpStatus.NOT_FOUND);
     }
 
     const query = this.incomeModel.find<Income>({ user, archived: false });
     if (limit) query.limit(limit);
     if (offset) query.skip(offset);
     if (sort) {
-      const [field, order] = sort.split(":");
-      if (order === "asc") query.sort({ [field]: 1 });
+      const [field, order] = sort.split(':');
+      if (order === 'asc') query.sort({ [field]: 1 });
       else query.sort({ [field]: -1 });
     }
 
@@ -52,44 +72,84 @@ export class IncomesService {
   }
 
   async findOne(id: string) {
-    const income = (await this.incomeModel.findById(id).exec()).toObject();
-    if (!income) throw new HttpException(IncomeErrorCodes.INCOME_NOT_FOUND, HttpStatus.NOT_FOUND);
+    const income = (await this.incomeModel.findById(id).exec())?.toObject();
+    if (!income)
+      throw new HttpException(
+        IncomeErrorCodes.INCOME_NOT_FOUND,
+        HttpStatus.NOT_FOUND,
+      );
 
     return new IncomeEntity(income);
   }
 
   async update(id: string, updateIncomeDto: UpdateIncomeDto) {
-    const income = (await this.incomeModel.findByIdAndUpdate(id, updateIncomeDto, { new: true }).exec()).toObject();
-    if (!income) throw new HttpException(IncomeErrorCodes.INCOME_NOT_FOUND, HttpStatus.NOT_FOUND);
+    const income = (
+      await this.incomeModel
+        .findByIdAndUpdate(id, updateIncomeDto, { new: true })
+        .exec()
+    )?.toObject();
+    if (!income)
+      throw new HttpException(
+        IncomeErrorCodes.INCOME_NOT_FOUND,
+        HttpStatus.NOT_FOUND,
+      );
 
     return new IncomeEntity(income);
   }
 
   async updateMany(updateManyIncomeDto: UpdateManyIncomeDto[]) {
-    return Promise.all(updateManyIncomeDto.map(async (i) => {
-      const exists = await this.incomeModel.exists({ _id: i._id });
-      if (!exists) throw new HttpException(IncomeErrorCodes.INCOME_NOT_FOUND, HttpStatus.NOT_FOUND);
+    return Promise.all(
+      updateManyIncomeDto.map(async (i) => {
+        const exists = await this.incomeModel.exists({ _id: i._id });
+        if (!exists)
+          throw new HttpException(
+            IncomeErrorCodes.INCOME_NOT_FOUND,
+            HttpStatus.NOT_FOUND,
+          );
 
-      const income = (await this.incomeModel.findByIdAndUpdate(i._id, i, { new: true }).exec()).toObject();
-      return new IncomeEntity(income);
-    }));
+        const income = (
+          await this.incomeModel
+            .findByIdAndUpdate(i._id, i, { new: true })
+            .exec()
+        ).toObject();
+        return new IncomeEntity(income);
+      }),
+    );
   }
 
   async remove(id: string) {
     const exists = await this.incomeModel.exists({ _id: id });
-    if (!exists) throw new HttpException(IncomeErrorCodes.INCOME_NOT_FOUND, HttpStatus.NOT_FOUND);
+    if (!exists)
+      throw new HttpException(
+        IncomeErrorCodes.INCOME_NOT_FOUND,
+        HttpStatus.NOT_FOUND,
+      );
 
-    const income = (await this.incomeModel.findByIdAndUpdate(id, { archived: true }, { new: true }).exec()).toObject();
+    const income = (
+      await this.incomeModel
+        .findByIdAndUpdate(id, { archived: true }, { new: true })
+        .exec()
+    ).toObject();
     return new IncomeEntity(income);
   }
 
   async removeMany(ids: string[]) {
-    return Promise.all(ids.map(async (id) => {
-      const exists = await this.incomeModel.exists({ _id: id });
-      if (!exists) throw new HttpException(IncomeErrorCodes.INCOME_NOT_FOUND, HttpStatus.NOT_FOUND);
+    return Promise.all(
+      ids.map(async (id) => {
+        const exists = await this.incomeModel.exists({ _id: id });
+        if (!exists)
+          throw new HttpException(
+            IncomeErrorCodes.INCOME_NOT_FOUND,
+            HttpStatus.NOT_FOUND,
+          );
 
-      const income = (await this.incomeModel.findByIdAndUpdate(id, { archived: true }, { new: true }).exec()).toObject();
-      return new IncomeEntity(income);
-    }));
+        const income = (
+          await this.incomeModel
+            .findByIdAndUpdate(id, { archived: true }, { new: true })
+            .exec()
+        ).toObject();
+        return new IncomeEntity(income);
+      }),
+    );
   }
 }
